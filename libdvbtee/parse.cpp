ec1f92cffcacaf70191bb135d49749388581b1b5
Michael Krufky <mkrufky@linuxtv.org>
2012-09-02 11:41:16 -0400

    parse: simplify test for payload pids containing elementary streams
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>

--- libdvbtee/parse.cpp
+++ libdvbtee/parse.cpp
@@ -965,9 +965,13 @@ int parse::feed(int count, uint8_t* p_data)
 				break;
 			}
 
+#if 0
 			map_pidtype::const_iterator iter_payload;
 			iter_payload = payload_pids.find(pid);
 			if (iter_payload != payload_pids.end()) {
+#else
+			if (payload_pids.count(pid)) {
+#endif
 				send_pkt = true;
 				out_type = OUTPUT_PES;
 				break;
