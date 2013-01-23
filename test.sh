85785b3d25cf9cadb6c5b47046cfb9c454830c28
Michael Krufky <mkrufky@linuxtv.org>
2013-01-22 20:40:18 -0500

    fix LD_LIBRARY_PATH order to include the build directories first
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-LD_LIBRARY_PATH=usr/lib:libdvbtee:libdvbtee_server dvbtee/dvbtee $1 $2 $3 $4 $5 $6 $7 $8 $9
+LD_LIBRARY_PATH=libdvbtee:libdvbtee_server:usr/lib dvbtee/dvbtee $1 $2 $3 $4 $5 $6 $7 $8 $9
