cd4a5dbf3c59e40bef2313e57b62bfce88909e53
Michael Krufky <mkrufky@linuxtv.org>
2013-05-20 11:02:13 -0400

    desc: don't process descriptor data if decoder failed
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>

--- libdvbtee/desc.cpp
+++ libdvbtee/desc.cpp
@@ -70,6 +70,7 @@ bool desc::iso639language(dvbpsi_descriptor_t* p_descriptor)
 		return false;
 
 	dvbpsi_iso639_dr_t* dr = dvbpsi_DecodeISO639Dr(p_descriptor);
+	if (!dr) return false;
 
 	for (int i = 0; i < dr->i_code_count; ++i)
 		dprintf("%c%c%c %x",
@@ -87,6 +88,7 @@ bool desc::service(dvbpsi_descriptor_t* p_descriptor)
 		return false;
 
 	dvbpsi_service_dr_t* dr = dvbpsi_DecodeServiceDr(p_descriptor);
+	if (!dr) return false;
 
 	get_descriptor_text(dr->i_service_provider_name, dr->i_service_provider_name_length, provider_name);
 	get_descriptor_text(dr->i_service_name,          dr->i_service_name_length,          service_name);
@@ -102,6 +104,7 @@ bool desc::short_event(dvbpsi_descriptor_t* p_descriptor)
 		return false;
 
 	dvbpsi_short_event_dr_t* dr = dvbpsi_DecodeShortEventDr(p_descriptor);
+	if (!dr) return false;
 
 	memcpy(_4d.lang, dr->i_iso_639_code, 3);
 	get_descriptor_text(dr->i_event_name, dr->i_event_name_length, _4d.name);
@@ -119,6 +122,8 @@ bool desc::freq_list(dvbpsi_descriptor_t* p_descriptor)
 		return false;
 
 	dvbpsi_frequency_list_dr_t* dr = dvbpsi_DecodeFrequencyListDr(p_descriptor);
+	if (!dr) return false;
+
 	for (int i = 0; i < dr->i_number_of_frequencies; ++i) {
 #if 0
 		= dr->p_center_frequencies[i]
@@ -135,6 +140,8 @@ bool desc::_lcn(dvbpsi_descriptor_t* p_descriptor)
 		return false;
 
 	dvbpsi_lcn_dr_t* dr = dvbpsi_DecodeLCNDr(p_descriptor);
+	if (!dr) return false;
+
 	for (int i = 0; i < dr->i_number_of_entries; i ++) {
 #if 0
 		= lcn->p_entries[i].i_service_id;
@@ -154,6 +161,8 @@ bool desc::caption_service(dvbpsi_descriptor_t* p_descriptor)
 		return false;
 
 	dvbpsi_caption_service_dr_t* dr = dvbpsi_DecodeCaptionServiceDr(p_descriptor);
+	if (!dr) return false;
+
 	dvbpsi_caption_service_t *service = dr->p_first_service;
 	for (int i = 0; i < dr->i_number_of_services; i ++) {
 		if (!service) {
@@ -183,6 +192,7 @@ bool desc::extended_channel_name(dvbpsi_descriptor_t* p_descriptor)
 		return false;
 
 	dvbpsi_extended_channel_name_dr_t *dr = dvbpsi_ExtendedChannelNameeDr(p_descriptor);
+	if (!dr) return false;
 
 	unsigned char name[256];
 	memset(name, 0, sizeof(name));
@@ -199,6 +209,8 @@ bool desc::service_location(dvbpsi_descriptor_t* p_descriptor)
 		return false;
 
 	dvbpsi_service_location_dr_t* dr = dvbpsi_DecodeServiceLocationDr(p_descriptor);
+	if (!dr) return false;
+
 	dvbpsi_service_location_element_t *element = dr->p_first_element;
 	for (int i = 0; i < dr->i_number_elements; i ++) {
 		if (!element) {
