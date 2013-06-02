3413b55cc44b088d874ccb61697e695656a46272
Michael Krufky <mkrufky@linuxtv.org>
2013-06-02 12:00:38 -0400

    dr_a0: rename dvbpsi_ExtendedChannelNameeDr to dvbpsi_ExtendedChannelNameDr
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>

--- libdvbtee/desc.cpp
+++ libdvbtee/desc.cpp
@@ -339,7 +339,7 @@ bool desc::extended_channel_name(dvbpsi_descriptor_t* p_descriptor)
 	if (p_descriptor->i_tag != DT_ExtendedChannelName)
 		return false;
 
-	dvbpsi_extended_channel_name_dr_t *dr = dvbpsi_ExtendedChannelNameeDr(p_descriptor);
+	dvbpsi_extended_channel_name_dr_t *dr = dvbpsi_ExtendedChannelNameDr(p_descriptor);
 	if (desc_dr_failed(dr)) return false;
 
 	unsigned char name[256];
