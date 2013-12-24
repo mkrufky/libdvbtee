7fd5c8afa189d9209c8ea7e4844eed43ccb0d0a5
Michael Krufky <mkrufky@linuxtv.org>
2013-12-24 11:53:08 -0500

    build.sh: use my libdvbpsi new_descriptors branch

--- build.sh
+++ build.sh
@@ -42,7 +42,7 @@ if [ -e libdvbpsi ]; then
 else
     git clone git://github.com/mkrufky/libdvbpsi.git
     cd libdvbpsi
-    git checkout descriptors
+    git checkout new_descriptors
     patch -p2 < ../libdvbpsi-silence-TS-discontinuity-messages.patch
 fi
 
