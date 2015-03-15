63ec1067d860a868e0faf5f7f17b43af20fc3066
Michael Ira Krufky <mkrufky@linuxtv.org>
2015-03-15 13:54:28 -0400

    descriptor/desc_a0: set fieldname to extChannelName rather than name
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>

--- libdvbtee/decode/descriptor/desc_a0.cpp
+++ libdvbtee/decode/descriptor/desc_a0.cpp
@@ -47,8 +47,8 @@ desc_a0::desc_a0(Decoder *parent, dvbpsi_descriptor_t *p_descriptor)
 	memset(name, 0, sizeof(name));
 	decode_multiple_string(dr->i_long_channel_name, dr->i_long_channel_name_length, name, sizeof(name));
 
-	set("name", std::string((const char *)name));
-	dprintf("%s", get<std::string>("name").c_str());
+	set("extChannelName", std::string((const char *)name));
+	dprintf("%s", get<std::string>("extChannelName").c_str());
 
 	setValid(true);
 }
