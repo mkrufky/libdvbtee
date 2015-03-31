18f84014f7634869512e0258be5c26beead575f4
Michael Ira Krufky <mkrufky@linuxtv.org>
2015-03-30 21:21:32 -0400

    value: reorganize macros
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>

--- libdvbtee/decode/value/value-macros.h
+++ libdvbtee/decode/value/value-macros.h
@@ -22,20 +22,6 @@
 #ifndef __VALUE_MACROS_H__
 #define __VALUE_MACROS_H__
 
-#define GET_DEFAULT(CLASS, IN, OUT) \
-template <> const OUT& CLASS::get<OUT>(IN in) const \
-{ \
-	OUT def; \
-	return get<OUT>(in, def); \
-}
-
-#define GET_DEFAULT_VALUE(CLASS, IN, OUT, DEFAULT) \
-template <> const OUT& CLASS::get<OUT>(IN in) const \
-{ \
-	OUT def = DEFAULT; \
-	return get<OUT>(in, def); \
-}
-
 #define TO_JSON_VALUE_TPL(CLASS, EXPRESSION) \
 namespace dvbtee { \
 namespace decode { \
@@ -49,6 +35,20 @@ template <> const std::string Value<CLASS>::toJsonValue() const \
 }}
 
 #define TO_JSON_VALUE_TPL_PRIMITIVE(CLASS) TO_JSON_VALUE_TPL(CLASS, m_value)
+
+#define GET_DEFAULT(CLASS, IN, OUT) \
+template <> const OUT& CLASS::get<OUT>(IN in) const \
+{ \
+	OUT def; \
+	return get<OUT>(in, def); \
+}
+
+#define GET_DEFAULT_VALUE(CLASS, IN, OUT, DEFAULT) \
+template <> const OUT& CLASS::get<OUT>(IN in) const \
+{ \
+	OUT def = DEFAULT; \
+	return get<OUT>(in, def); \
+}
 
 #define DEFINE_DEFAULT_GETTERS(CLASS, IN) \
 namespace dvbtee { \
