2f869ae8db74ad1319fd3e5bcfde8cd448192b93
Michael Ira Krufky <mkrufky@linuxtv.org>
2015-07-13 19:13:06 -0400

    hlsfeed: add explicit braces to avoid dangling else
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>

--- libdvbtee/hlsfeed.cpp
+++ libdvbtee/hlsfeed.cpp
@@ -203,8 +203,8 @@ void* hlsfeed::walk_thread()
 void hlsfeed::push(uint8_t *buffer, size_t size, size_t nmemb)
 {
 #if PUSH_THREAD
-  if (!push_buffer.write(buffer, size * nmemb))
-    while (nmemb)
+  if (!push_buffer.write(buffer, size * nmemb)) {
+    while (nmemb) {
       if (push_buffer.write(buffer, nmemb)) {
 	buffer += size;
 	nmemb--;
@@ -212,6 +212,8 @@ void hlsfeed::push(uint8_t *buffer, size_t size, size_t nmemb)
 	fprintf(stderr, "%s: FAILED: %zu packets dropped\n", __func__, nmemb);
 	return;
       }
+    }
+  }
 #else
   if (datapump_cb)
     datapump_cb(datapump_ctxt, buffer, size, nmemb);
@@ -221,8 +223,8 @@ void hlsfeed::push(uint8_t *buffer, size_t size, size_t nmemb)
 void hlsfeed::walk(uint8_t *buffer, size_t size, size_t nmemb)
 {
 #if WALK_THREAD
-  if (!walk_buffer.write(buffer, size * nmemb))
-    while (nmemb)
+  if (!walk_buffer.write(buffer, size * nmemb)) {
+    while (nmemb) {
       if (walk_buffer.write(buffer, nmemb)) {
 	buffer += size;
 	nmemb--;
@@ -230,6 +232,8 @@ void hlsfeed::walk(uint8_t *buffer, size_t size, size_t nmemb)
 	fprintf(stderr, "%s: FAILED: %zu bytes dropped\n", __func__, size * nmemb);
 	return;
       }
+    }
+  }
 #else
   walk(buffer);
 #endif
