0dd3116458271124eb2616c90e00c40deb6f1c72
Michael Ira Krufky <mkrufky@linuxtv.org>
2015-08-02 15:39:06 -0400

    decode/table: fix warning: format '%ld' expects argument of type 'long int',
    but argument 3 has type 'ssize_t {aka int}' [-Wformat=]
    
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>

--- libdvbtee/decode/table/table.h
+++ libdvbtee/decode/table/table.h
@@ -124,7 +124,7 @@ public:
 	{
 		std::vector<Table*> V = get(tableid);
 		ssize_t s = V.size();
-		if (s > 1) printf("TABLE: %02x %ld collected, something is wrong\n", tableid, s);
+		if (s > 1) printf("TABLE: %02x %zu collected, something is wrong\n", tableid, s);
 		if (s) {
 			printf("UPDATING TABLE %02x\n", tableid);
 			C *t = (C*)V[s-1];
