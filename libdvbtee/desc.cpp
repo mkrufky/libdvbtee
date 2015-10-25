c33d8a0a6f910b394020f890fe2f51efaf8f115b
Michael Ira Krufky <mkrufky@linuxtv.org>
2015-10-25 16:06:51 -0400

    desc: fix build breakage against libdvbpsi 2.0.0-git
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>

--- libdvbtee/desc.cpp
+++ libdvbtee/desc.cpp
@@ -357,7 +357,7 @@ bool desc::extended_channel_name(dvbpsi_descriptor_t* p_descriptor)
 	if (p_descriptor->i_tag != DT_ExtendedChannelName)
 		return false;
 
-#if (DVBPSI_VERSION_INT < ((2<<16)+(0<<8)+0))
+#if (DVBPSI_VERSION_INT <= ((2<<16)+(0<<8)+0))
 #define dvbpsi_DecodeExtendedChannelNameDr dvbpsi_ExtendedChannelNameDr
 #endif
 	dvbpsi_extended_channel_name_dr_t *dr = dvbpsi_DecodeExtendedChannelNameDr(p_descriptor);
