1a36951fe86def9f3534d49c492b8ee8ddc195c5
Michael Ira Krufky <mkrufky@linuxtv.org>
2015-12-12 12:18:05 -0500

    build-auto.sh: build against latest libdvbpsi using autotools
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>

---
+++ build-auto.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+export DVBTEE_ROOT="`pwd`"
+
+mkdir -p usr/bin
+mkdir -p usr/lib
+mkdir -p usr/include
+
+if [ -e .clean ]; then
+    make clean -C libdvbpsi
+    if [ $? != 0 ]; then
+        echo "make clean (libdvbpsi) failed"
+        #exit 1 // dont exit
+    fi
+
+    make clean -C dvbtee
+    if [ $? != 0 ]; then
+        echo "make clean (dvbtee) failed"
+        exit 1
+    fi
+fi
+
+if [ -e libdvbpsi/bootstrap ]; then
+    cd libdvbpsi
+else
+    rm -rf libdvbpsi
+    git clone git://github.com/mkrufky/libdvbpsi.git
+    cd libdvbpsi
+    touch .dont_del
+fi
+
+./bootstrap
+./configure --prefix=${DVBTEE_ROOT}/usr/ --enable-static
+cd ..
+
+make -C libdvbpsi -j
+if [ $? != 0 ]; then
+    echo "make (libdvbpsi) failed"
+    exit 1
+fi
+
+make -C libdvbpsi install
+
+mkdir -p m4
+autoreconf --install
+
+if [ -e .staticlib ]; then
+    ./configure CPPFLAGS=-I${DVBTEE_ROOT}/usr/include/dvbpsi/ LDFLAGS="-L${DVBTEE_ROOT}/usr/lib/ -ldvbpsi" --enable-dvbpsibuild --enable-static
+else
+    ./configure CPPFLAGS=-I${DVBTEE_ROOT}/usr/include/dvbpsi/ LDFLAGS="-L${DVBTEE_ROOT}/usr/lib/ -ldvbpsi" --enable-dvbpsibuild
+fi
+
+make -C . -j
+if [ $? != 0 ]; then
+    echo "make (dvbtee) failed"
+    exit 1
+fi
+
+make -C dvbtee
