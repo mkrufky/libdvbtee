8d9ab16d9bd6d655888e892bbb3395c87fe351cb
Michael Ira Krufky <mkrufky@linuxtv.org>
2016-02-14 08:41:37 -0500

    examples/tests: blame it on the stream
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>

--- examples/tcplisten/tests.sh
+++ examples/tcplisten/tests.sh
@@ -1,1 +1,1 @@
-./dvbtee-tcplisten -p1234 -t12 -d & sleep 1 && ../../dvbtee/dvbtee -F../../football.ts -otcp:localhost:1234 -t5 2> /dev/null
+./dvbtee-tcplisten -p1234 -t12 -d & sleep 1 && ../../dvbtee/dvbtee -F../../sbs.2.5M.ts -otcp:localhost:1234 -t5 2> /dev/null
--- examples/udplisten/tests.sh
+++ examples/udplisten/tests.sh
@@ -1,1 +1,1 @@
-./dvbtee-udplisten -p1234 -t12 -d & sleep 1 && ../../dvbtee/dvbtee -F../../football.ts -oudp:localhost:1234 -t5 2> /dev/null
+./dvbtee-udplisten -p1234 -t12 -d & sleep 1 && ../../dvbtee/dvbtee -F../../sbs.2.5M.ts -oudp:localhost:1234 -t5 2> /dev/null
