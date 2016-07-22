5d907042d613eaf699070fce491e7973398bfb0f
Michael Ira Krufky <mkrufky@linuxtv.org>
2016-07-22 11:20:05 -0400

    log: fix error: unable to find string literal operator
    fix error: unable to find string literal operator 'operator""CLASS_MODULE' with 'const char [13]', 'unsigned int' arguments
    
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>

--- libdvbtee/log.h
+++ libdvbtee/log.h
@@ -47,7 +47,7 @@ void libdvbtee_set_debug_level(unsigned int debug);
 
 #define __dPrintf(lvl, fmt, arg...) do {				\
 	if (dbg & lvl)							\
-		__printf(stderr, "%d "CLASS_MODULE"::%s: " fmt "\n", 	\
+		__printf(stderr, "%d " CLASS_MODULE "::%s: " fmt "\n", 	\
 				 (int)time(NULL), __func__, ##arg);	\
 } while (0)
 
