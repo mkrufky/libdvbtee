1083c6967eace410da9c9ae1a23cdd37ed5de1db
Michael Ira Krufky <mkrufky@linuxtv.org>
2017-02-26 07:43:02 -0500

    log: fix unintentional superfluous quotes in logs
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>

--- libdvbtee/log.h
+++ libdvbtee/log.h
@@ -53,7 +53,7 @@ int libdvbtee_set_debug_level(unsigned int debug, unsigned int debug_info = 1);
 
 #define __dPrintf(lvl, fmt, arg...) do {				\
 	if (dbg & lvl)							\
-		__printf(stderr, "%d %s::%s: " #fmt "\n",		\
+		__printf(stderr, "%d %s::%s: " fmt "\n",		\
 			 (int)time(NULL), CLASS_MODULE, __func__, ##arg);	\
 } while (0)
 
